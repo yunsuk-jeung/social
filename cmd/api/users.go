ea4dcfb1eff5502bc67a66d64c082a275dd1802f
ysjeung <yunsukjeung@snu.ac.kr>
2025-05-18 17:53:39 +0900

    fix: remove unused func

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -163,34 +163,34 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 
 }
 
-func (app *application) userContextMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		idParam := chi.URLParam(r, "userID")
-		id, err := strconv.ParseInt(idParam, 10, 64)
-
-		if err != nil {
-			app.internalServerError(w, r, err)
-			return
-		}
-
-		ctx := r.Context()
-
-		user, err := app.store.Users.GetByID(ctx, id)
-
-		if err != nil {
-			switch err {
-			case store.ErrNotFound:
-				app.notFoundResponse(w, r, err)
-				return
-			default:
-				app.internalServerError(w, r, err)
-				return
-			}
-		}
-		ctx = context.WithValue(ctx, userCtx, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+// func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		idParam := chi.URLParam(r, "userID")
+// 		id, err := strconv.ParseInt(idParam, 10, 64)
+//
+// 		if err != nil {
+// 			app.internalServerError(w, r, err)
+// 			return
+// 		}
+//
+// 		ctx := r.Context()
+//
+// 		user, err := app.store.Users.GetByID(ctx, id)
+//
+// 		if err != nil {
+// 			switch err {
+// 			case store.ErrNotFound:
+// 				app.notFoundResponse(w, r, err)
+// 				return
+// 			default:
+// 				app.internalServerError(w, r, err)
+// 				return
+// 			}
+// 		}
+// 		ctx = context.WithValue(ctx, userCtx, user)
+// 		next.ServeHTTP(w, r.WithContext(ctx))
+// 	})
+// }
 
 func getUserFromCtx(r *http.Request) *store.User {
 	user, _ := r.Context().Value(userCtx).(*store.User)
--- internal/store/mocks.go
+++ internal/store/mocks.go
@@ -30,20 +30,20 @@ func (s *MockUserStore) CreateAndInvite(ctx context.Context, user *User, token s
 
 func (s *MockUserStore) Activate(ctx context.Context, token string) error { return nil }
 
-func (s *MockUserStore) getUserFromInvitation(ctx context.Context, tx *sql.Tx, token string) (*User, error) {
-	return &User{}, nil
-}
-
-func (s *MockUserStore) createUserInvitation(ctx context.Context, tx *sql.Tx, token string, exp time.Duration, userID int64) error {
-	return nil
-}
-
-func (s *MockUserStore) update(ctx context.Context, tx *sql.Tx, user *User) error { return nil }
-
-func (s *MockUserStore) deleteUserInvitations(ctx context.Context, tx *sql.Tx, userID int64) error {
-	return nil
-}
+// func (s *MockUserStore) getUserFromInvitation(ctx context.Context, tx *sql.Tx, token string) (*User, error) {
+// 	return &User{}, nil
+// }
+//
+// func (s *MockUserStore) createUserInvitation(ctx context.Context, tx *sql.Tx, token string, exp time.Duration, userID int64) error {
+// 	return nil
+// }
+//
+// func (s *MockUserStore) update(ctx context.Context, tx *sql.Tx, user *User) error { return nil }
+//
+// func (s *MockUserStore) deleteUserInvitations(ctx context.Context, tx *sql.Tx, userID int64) error {
+// 	return nil
+// }
 
 func (s *MockUserStore) Delete(ctx context.Context, userID int64) error { return nil }
 
-func (s *MockUserStore) delete(ctx context.Context, tx *sql.Tx, userID int64) error { return nil }
+// func (s *MockUserStore) delete(ctx context.Context, tx *sql.Tx, userID int64) error { return nil }
